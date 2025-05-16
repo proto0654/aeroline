ff42604659cc946a0cc793369642021f536ae9e5
Your Name <your-email@example.com>
2025-05-16 03:17:42 +0300

    Обновлена конфигурация Tailwind CSS для работы с новой структурой проекта

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,hbs}",
+    "./assets/**/*.{html,js,hbs}",
     "./*.html",
     "./main.js"
   ],
